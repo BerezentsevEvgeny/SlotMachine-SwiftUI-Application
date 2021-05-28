495907f0c6f173ba458626a1efe3484081d20378
Berezentsev Evgeny <jonn09@icloud.com>
2021-05-28 11:20:25 +0300

    Initial Commit

---
+++ SlotMachine/SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by Евгений on 04.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
